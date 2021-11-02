62a7118a9662ea229e6944df323c0a809a05972f
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-11-02 15:12:18 +0000

    機能完成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy(book.params)
+    redirect_to books_path
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+end
